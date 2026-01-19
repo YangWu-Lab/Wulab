be3b7586ae5cff574bfd37b73fd1dd6611cb9499
YangWu-Lab <liwenjian@wchscu.edu.cn>
2026-01-19 12:35:36 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,27 @@
-// vue.config.js
 const { defineConfig } = require("@vue/cli-service");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = defineConfig({
   transpileDependencies: true,
 
   chainWebpack: (config) => {
     config.module.rule("md").test(/\.md$/).type("asset/source");
+    
+    // 设置 HTML 页面标题
+    config.plugin("html").tap((args) => {
+      args[0].title = "Wu Lab"; // 这里设置页面标题为 "Wu Lab"
+      return args;
+    });
   },
 
-  // 你如果是 GitHub Pages 子路径才需要；本机/服务器一般是 "/"
   publicPath: process.env.NODE_ENV === "production" ? "/Wulab/" : "/",
 
-  // ✅ 关键：开发时把 /api 代理到本机 3001 或 8080
-  // 这里我们后端建议也用 3001（dev），prod 用 8080 也行
   devServer: {
     proxy: {
       "/api": {
         target: "http://localhost:3001",
-        changeOrigin: true
-      }
-    }
-  }
+        changeOrigin: true,
+      },
+    },
+  },
 });
